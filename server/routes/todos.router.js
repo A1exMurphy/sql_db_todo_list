8f6c24d3c7c1cbb2ec5341935802f20c234661bd
Evan Langer <elanger@live.com>
2023-11-12 17:44:01 -0600

    linked router file to server and db

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+//variables to define routes
+const todoRouter = express.Router();
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    hostname: 'localhost',
+    port: 5001,
+    database: 'weekend-to-do-app',
+})
+
+
+//GET for intial render and updating render
+todoRouter.get()
+
+//POST for adding task to db 
+
+
+//PUT to change status of tasks 
+
+
+//DELETE to remove a task from the list
 
 
 
